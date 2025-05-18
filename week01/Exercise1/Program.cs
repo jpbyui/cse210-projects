af5a3e68bb0a3fe81a0211656a080881692c37a4
jpbyui <juan.lds@gmail.com>
2025-05-17 22:37:38 -0300

    w01 done

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using System.Reflection.Emit;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+
+        String name;
+        String last_name;
+        Console.Write("What is your name?");
+        name = Console.ReadLine();
+        Console.Write("What is your last name?");
+        last_name = Console.ReadLine();
+        Console.WriteLine("Your name is " + last_name + ", " + name + " " + last_name, ".");
+
     }
 }
